e7176d89b8cf457bac036bde0f1ed341a1384ec1
irenaYo <irenatugushi997@gmail.com>
2026-06-12 18:52:43 +0300

    implement AmplFilter

---
+++ SoundProcessor/SoundProcessor/Filters/Effects/AmplFilter.cs
@@ -0,0 +1,25 @@
+using SoundProcessor.Audio;
+using SoundProcessor.Pipelines;
+
+namespace SoundProcessor.Filters.Effects;
+
+public class AmplFilter : IFilter
+{
+    private double Factor { get; }
+    
+    public AmplFilter(double factor)
+    {
+        Factor = factor;
+    }
+    
+    public State Apply(Waveform sound)
+    {
+        for (int i = 0; i < sound.Samples.Count; i++)
+        {
+            double operation = Math.Clamp(sound.Samples[i] * Factor, short.MinValue, short.MaxValue);
+            sound.Samples[i] = (short)Math.Round(operation);
+        }
+
+        return State.Ok;
+    }
+}
