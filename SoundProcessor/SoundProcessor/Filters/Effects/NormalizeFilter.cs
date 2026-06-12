c20f7b2f54afb86c8bc89cbee43bd51b738722a4
irenaYo <irenatugushi997@gmail.com>
2026-06-12 19:44:38 +0300

    implement NormalizeFilter

---
+++ SoundProcessor/SoundProcessor/Filters/Effects/NormalizeFilter.cs
@@ -0,0 +1,42 @@
+using SoundProcessor.Audio;
+using SoundProcessor.Pipelines;
+
+namespace SoundProcessor.Filters.Effects;
+
+public class NormalizeFilter : IFilter
+{
+    private double Peak { get; set; }
+
+    public NormalizeFilter(double peak)
+    {
+        Peak = peak;
+    }
+    
+    public NormalizeFilter()
+    {
+        Peak = 1;
+    }
+
+    public State Apply(Waveform sound)
+    {
+        if (sound.Samples == null)
+            throw new ArgumentNullException(nameof(sound));
+        
+        int currentPeak = sound.Samples
+            .Select(s => Math.Abs((int)s))
+            .Max();
+
+        if (currentPeak == 0)
+            return State.Ok;
+        
+        double scale = Peak * short.MaxValue / currentPeak;
+
+        for (int i = 0; i < sound.Samples.Count; i++)
+        {
+            double operation = Math.Clamp(sound.Samples[i] * scale, short.MinValue, short.MaxValue);
+            sound.Samples[i] = (short)Math.Round(operation);
+        }
+        
+        return State.Ok;
+    }
+}
