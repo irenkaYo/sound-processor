c5c3fefeb84bd5b110a35aee7af27b5c7ce97d29
irenaYo <irenatugushi997@gmail.com>
2026-06-16 13:09:01 +0300

    create classes to run program

---
+++ SoundProcessor/SoundProcessor/Application.cs
@@ -0,0 +1,53 @@
+using SoundProcessor.Audio;
+using SoundProcessor.CommandLine;
+using SoundProcessor.Converters;
+using SoundProcessor.Pipelines;
+
+namespace SoundProcessor;
+
+public class Application
+{
+    private CmdLineArgs2PipelineConverter _cmdConverter;
+
+    public Application(CmdLineArgs2PipelineConverter cmdConverter)
+    {
+        _cmdConverter = cmdConverter;
+    }
+
+    private void Configure()
+    {
+        _cmdConverter.AddFilterProducer("ampl", FilterProducers.AmplFilterCreator);
+        _cmdConverter.AddFilterProducer("normalize", FilterProducers.NormalizeFilterCreator);
+        _cmdConverter.AddFilterProducer("silence", FilterProducers.SilenceFilterCreator);
+        _cmdConverter.AddFilterProducer("timestretch", FilterProducers.TimestretchFilterCreator);
+        _cmdConverter.AddFilterProducer("lowpass", FilterProducers.LowpassFilterCreator);
+        _cmdConverter.AddFilterProducer("generator", FilterProducers.GeneratorFilterCreator);
+    }
+
+    public void Run(string[] args)
+    {
+        Configure();
+        ArgsParser parser = new ArgsParser(args);
+
+        ParseResult parseResult = parser.ParseArguments();
+        if (parseResult != ParseResult.Ok)
+            throw new Exception("Parsing error: " + parseResult);
+        
+        Pipeline pipeline = _cmdConverter.CreatePipeline(parser.Filters);
+
+        Waveform sound;
+        if (parser.InputFileName != null)
+            sound = WavReader.ReadFile(parser.InputFileName);
+        else
+            sound = new Waveform();
+        
+        State state = pipeline.Execute(sound);
+        
+        if (state != State.Ok)
+        {
+            throw new Exception($"Pipeline failed: {state}");
+        }
+        
+        WavWriter.WriteFile(parser.OutputFileName, sound);
+    }
+}
---
+++ SoundProcessor/SoundProcessor/Program.cs
@@ -0,0 +1,12 @@
+﻿using SoundProcessor;
+using SoundProcessor.Converters;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CmdLineArgs2PipelineConverter converter = new CmdLineArgs2PipelineConverter();
+        Application application = new Application(converter);
+        application.Run(args);
+    }
+}
