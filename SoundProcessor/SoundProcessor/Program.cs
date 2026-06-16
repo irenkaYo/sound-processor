58bdef7eba43a9dd15514549731cf55b2c1bc1df
irenaYo <irenatugushi997@gmail.com>
2026-06-16 16:25:06 +0300

    add top-level exception handling in Program

--- SoundProcessor/SoundProcessor/Program.cs
+++ SoundProcessor/SoundProcessor/Program.cs
@@ -5,8 +5,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        CmdLineArgs2PipelineConverter converter = new CmdLineArgs2PipelineConverter();
-        Application application = new Application(converter);
-        application.Run(args);
+        try
+        {
+            CmdLineArgs2PipelineConverter converter = new CmdLineArgs2PipelineConverter();
+            Application application = new Application(converter);
+            application.Run(args);
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine("Error: " + e.Message);
+            Environment.Exit(1);
+        }
     }
 }
